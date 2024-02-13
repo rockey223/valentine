b50c2a90b1161bcb6191ebf6318e684e68c38354
rockey223 <rockeym50@gmail.com>
2024-02-13 15:22:44 +0545

    device restriction

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,17 @@ import Valentine from './Valentine'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  if (window.innerWidth < 950) {
+    return (
+      <>
+        <p className='warning'>
+          Please note that this application is only compatible with desktop
+          devices. Please access it from a desktop computer for optimal
+          performance.
+        </p>
+      </>
+    );
+  }
   return (
     <>
       <Valentine/>
--- src/Valentine.jsx
+++ src/Valentine.jsx
@@ -7,24 +7,23 @@ const Valentine = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const ref = useRef(null);
 
-  const [rectt, setyRectt] = useState()
+  const [rectt, setyRectt] = useState();
 
   useEffect(() => {
-    const rect = ref.current.getBoundingClientRect();         
+    const rect = ref.current.getBoundingClientRect();
     console.log(rect.x);
-    setyRectt(rect.x)
+    setyRectt(rect.x);
     setPosition({ x: rect.x, y: rect.y });
     console.log(rect);
     console.log(window.innerHeight);
     // console.log(window.innerWidth);
     console.log(Math.random() * window.innerHeight);
   }, []);
-  
-  const getPos = () =>{
-    
+
+  const getPos = () => {
     const rect = ref.current.getBoundingClientRect();
     setPosition({ x: rect.x, y: rect.y });
-  }
+  };
 
   const no = () => {
     const buttonWidth = 150; // Adjust according to your button's width
@@ -38,6 +37,7 @@ const Valentine = () => {
     setPosition({ x: randomX, y: randomY });
   };
 
+ 
   return (
     <>
       <div className="card">
@@ -72,7 +72,13 @@ const Valentine = () => {
           " "
         ) : (
           <button
-            style={{ top: `${position.y }px`, left: `${position.x}px`, transition: `${rectt != position.x ? 'all 0.5s linear':"all 0s linear " }` }}
+            style={{
+              top: `${position.y}px`,
+              left: `${position.x}px`,
+              transition: `${
+                rectt != position.x ? "all 0.5s linear" : "all 0s linear "
+              }`,
+            }}
             onMouseEnter={no}
             // style={{ top: `${position.y}px`, right: `${position.x}px` }}
             className="Noo"
